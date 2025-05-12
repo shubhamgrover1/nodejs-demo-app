ac6787513c4f3e15fc35ddcb12269b3d38c5f14f
shubhamgrover1 <93376959+shubhamgrover1@users.noreply.github.com>
2025-05-12 17:53:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.end('Hello from Node.js App running inside Docker!');
+});
+
+server.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
 
